06f6c61c41bc47574c8f2409386c139eb46a3395
Gabriela Braga <bragaga@csc-scc.gc.ca>
2020-10-08 10:58:25 -0400

    created add points form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
@@ -8,7 +19,7 @@ import { PointsComponent } from './points/points.component';
 import { ContributionHistoryComponent } from './contribution-history/contribution-history.component';
 import { LoginComponent } from './login/login.component';
 import { AccountManagementComponent } from './account-management/account-management.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +33,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/points/points.component.ts
+++ src/app/points/points.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-points',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./points.component.css']
 })
 export class PointsComponent implements OnInit {
+  memberControl = new FormControl('');
+  houseControl = new FormControl('');
+  filteredMembers: Observable<string[]>;
+  paws: string[];
+  members: string[];
+  selectedMember: any;
 
-  constructor() { }
+  constructor() {
+    this.houseControl.disable();
+    this.paws = [
+      "Reason1",
+      "Reason2",
+      "Reason3",
+      "Reason4",
+      "Reason5",
+      "Reason6"
+    ];
+    this.members = [
+      "student1",
+      "student2",
+      "student3",
+      "student4",
+      "student5",
+      "student6"
+    ];
+  }
 
   ngOnInit(): void {
+    this.filteredMembers = this.memberControl.valueChanges.pipe(startWith(''),map(value => this._filter(value)));
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.paws.filter(option => option.toLowerCase().includes(filterValue));
   }
 
 }
