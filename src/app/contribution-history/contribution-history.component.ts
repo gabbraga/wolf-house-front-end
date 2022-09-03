581261ea170c9f0f34cb2d1ab3e6244cbd76590b
Gabriela Braga <bragaga@csc-scc.gc.ca>
2022-09-03 17:01:24 -0400

    refactor student contribution html

--- src/app/contribution-history/contribution-history.component.ts
+++ src/app/contribution-history/contribution-history.component.ts
@@ -7,9 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContributionHistoryComponent implements OnInit {
 
-  constructor() { }
+  public student: {
+    name: string;
+    house: string;
+  };
+
+  public pointsSubmissions: {
+    date: string;
+    points: number;
+    paw: string;
+    comments: string;
+    staff: string;
+  }[];
+  public totalPoints: number;
+
+  constructor() { 
+    this.student = {
+      name: 'Max',
+      house: 'Arctic'
+    }
+    this.pointsSubmissions = [
+      {
+        date: '12/03/2022',
+        points: 10,
+        paw: 'Honesty',
+        comments: 'best policy',
+        staff: 'Brock'
+      },
+      {
+        date: '11/02/2022',
+        points: 15,
+        paw: 'Perseverence',
+        comments: '10/10 persevered',
+        staff: 'Bonnie'
+      },
+      {
+        date: '08/13/2022',
+        points: 5,
+        paw: 'Kindness',
+        comments: 'so kind omg',
+        staff: 'Scott'
+      }
+    ];
+    this.totalPoints = this.getTotalPoints();
+  }
 
   ngOnInit(): void {
+  }
+
+  private getTotalPoints(): number {
+    let totalPoints = 0;
+    for (const submission of this.pointsSubmissions) {
+      totalPoints += submission.points;
+    }
+    return totalPoints;
   }
 
 }
